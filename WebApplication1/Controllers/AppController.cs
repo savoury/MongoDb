2d8e2cfd44bd6f0299a3a298a908bf7ecd1cf5b4
Romain Vacquerie <romain.vacquerie.k2partneringsolutions@axa-cs.com>
2018-10-24 17:36:47 +0200

    clean the mess

--- WebApplication1/Controllers/AppController.cs
+++ WebApplication1/Controllers/AppController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using WebApplication1.Service;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers
 {
