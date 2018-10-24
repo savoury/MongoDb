7f41b73c6c5bc649e6ac68d0b03d5b107d88b114
Romain Vacquerie <romain.vacquerie.k2partneringsolutions@axa-cs.com>
2018-10-24 17:12:24 +0200

    Add the poc

---
+++ WebApplication1/WebApplication1/Data/Mongo/GameContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data.Mongo.Domain;
+
+namespace WebApplication1.Data.Mongo
+{
+    public class GameContext : IGameContext
+    {
+        private readonly IMongoDatabase _db;
+        public GameContext(IOptions<Settings> options)
+        {
+            var client = new MongoClient(options.Value.ConnectionString);
+            _db = client.GetDatabase(options.Value.Database);
+            _db.RunCommandAsync((Command<BsonDocument>)"{ping:1}")
+        .Wait();
+        }
+        public IMongoCollection<Game> Games => _db.GetCollection<Game>("Games");
+    }
+
+    public interface IGameContext
+    {
+        IMongoCollection<Game> Games { get; }
+    }
+}
+
---
+++ WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Data.Mongo.Repository;
+using WebApplication1.Data.Mongo;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
+
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("DefaultConnection"))
+            //        );
+
+            //services.AddDbContext<ApplicationDbContext>();
+            //services.AddDefaultIdentity<IdentityUser>()
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.Configure<Settings>(options =>
+            {
+                options.ConnectionString = Configuration.GetSection("MongoDb:ConnectionString").Value;
+                options.Database = Configuration.GetSection("MongoDb:Database").Value;
+            });
+
+            services.AddTransient<IGameRepository, GameRepository>();
+            services.AddTransient<IGameContext, GameContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+            app.UseMvc();
+            //app.UseMvc(cfg =>
+            //{
+            //    cfg.MapRoute("Default",
+            //        "/{controller}/{action}/{id?}",
+            //        new { controller = "App", Action = "Index" });
+            //});
+        }
+    }
+
+    public class Settings
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+}
